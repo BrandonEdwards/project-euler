bbacbadc1d7e65870ec02736529689d35ba825b0
Brandon Edwards <brandon.edwards144@gmail.com>
2016-06-05 21:40:24 -0400

    Removing old files

--- Problem 10/p10.jl
+++
@@ -1,9 +0,0 @@
-sum = 0
-
-for i = 1:2000000
-  if isprime(i)
-    sum += i
-  end
-end
-
-sum
--- Problem 13/p13.jl
+++
@@ -1,7 +0,0 @@
-numbers = readdlm("numbers")
-sum = 0
-for i = 1:length(numbers)
-  sum = sum + numbers[i]
-end
-
-sum
